d5803d45449a99604165df3560bd95c6dbcc7a48
bluesky2008 <william_billl2008@yahoo.co.jp>
2022-05-17 13:17:55 +0900

    修正

--- resources/views/access.blade.php
+++ resources/views/access.blade.php
@@ -18,11 +18,12 @@
                 <span class="access_span">電車でお越しの方</span>JR東西線北新地駅
             </div>
         </div>
+        <table class="access_table">
+            <tr><th style="border-right:1px dashed #FFF;">営業時間</th><th>定休日</th></tr>
+            <tr><td>月～土 PM7:00～LAST</td><td>日曜・祝日</td></tr>
+        </table>
     </div>
-    <table class="access_table">
-        <tr><th style="border-right:1px dashed #FFF;">営業時間</th><th>定休日</th></tr>
-        <tr><td>月～土 PM7:00～LAST</td><td>日曜・祝日</td></tr>
-    </table>
+
     <div class="padding_50">
     <iframe style="margin: auto; display: block; border:0;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3280.352315310535!2d135.4969557155322!3d34.696292690961485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000e6ee7c29c817%3A0xd3a80e02a4b5eaf4!2z44CSNTMwLTAwMDMg5aSn6Ziq5bqc5aSn6Ziq5biC5YyX5Yy65aCC5bO277yR5LiB55uu77yS4oiS77yX!5e0!3m2!1sja!2sjp!4v1652246916607!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
